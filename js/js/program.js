23f2463af425598e2a2b013a2b8968f8e699d26c
Mohammad Rahal <rahalmohamad21@gmail.com>
2024-05-04 20:01:00 +0300

    coding

--- js/js/program.js
+++ js/js/program.js
@@ -1,16 +1,29 @@
 // at this program i made a loop to sort the array assending order
 // to desasse order if(array[j] < array[j +1] )
 
-const array = ["e", "d", "c", "b", "a"];
-for (let i = 0; i <= array.length; i++) {
-  for (let j = 0; j < array.length - 1; j++) {
-    if (array[j] > array[j + 1]) {
+// const array = ["e", "d", "c", "b", "a"];
+// for (let i = 0; i <= array.length; i++) {
+//   for (let j = 0; j < array.length - 1; j++) {
+//     if (array[j] > array[j + 1]) {
         // to swap the array data i used temp
-      let temp = array[j];
-      array[j] = array[j + 1];
-      array[j + 1] = temp;
-    }
-  }
-}
+//       let temp = array[j];
+//       array[j] = array[j + 1];
+//       array[j + 1] = temp;
+//     }
+//   }
+// }
+// or by using sort()
+// array.sort()
 
-console.log(array);
+// console.log(array, "using loops");
+// console.log(array , "using sort function");
+
+const num =0;
+if(num == 0){
+console.log('number is is equal to zero ')
+} else if(num > 0){
+  console.log('number is positive ', num)
+}else{
+  console.log('number is negative ', num)
+}
+return 0
